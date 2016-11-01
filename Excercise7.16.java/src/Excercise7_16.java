32932321b03c69d00d4a821fe18343123b08c4ac
Evert <JE640817@swosu.edu>
2016-11-01 15:49:28 -0500

    fixed the bracket issue on 7.16.

--- Excercise7.16.java/src/Excercise7_16.java
+++ Excercise7.16.java/src/Excercise7_16.java
@@ -1,30 +1,30 @@
 
 public class Excercise7_16 {
 
-//Main Method
+	//Main Method
 	public static void main(String[] args) {
 		// Randomly generates an array of 100,000
 		//integers and a key
-		
+
 		int num;
 		int [] numbers = new int [10000];
-		
+
 		for (int i = 0; i < numbers.length; i++){
 			numbers [i] = getRandomInt ();
 		}
-		
+
 		int key = getRandomInt ();
-		
+
 		//Estimate the execution time of invoking the linearSearch
 		int searchResult;
 		long startTime = System.currentTimeMillis();
 		searchResult = linearsearch(numbers, key);
 		long endTime = System.currentTimeMillis();
 		long executionTime = endTime - startTime;
-		
+
 		System.out.println("Execution time of invoking the linearsearch in  milliseconds: "  + executionTime );
-		
-		
+
+
 		//sort Array
 		selectionSort(numbers);
 
@@ -34,67 +34,64 @@ public class Excercise7_16 {
 		endTime = System.currentTimeMillis();
 		executionTime = endTime - startTime;
 		System.out.println(
-			"Execution time of invoking the binarySearch in milliseconds: " 
-			+ executionTime + " ");
+				"Execution time of invoking the binarySearch in milliseconds: " 
+						+ executionTime + " ");
 	}
-		
-		
-		//Returns getRandomInt
+
+
+	//Returns getRandomInt
 	private static int getRandomInt() {
 		return 1 + (int)(Math.random() * 100000);
 	}
-		
-		//Returns linearSearch
-		private static int linearsearch(int[] array, int key) {
-			for (int i = 0; i < array.length; i++) {
-				if (key == array[i])
-					return i;
-			}
+
+	//Returns linearSearch
+	private static int linearsearch(int[] array, int key) {
+		for (int i = 0; i < array.length; i++) {
+			if (key == array[i])
+				return i;
+		}
 		return -1;
+	}
+
+	//Returns binarySearch
+
+	private static int binarySearch(int[] array, int key) {
+		int low = 0;
+		int high = array.length - 1;
+
+		while (high >= low) {
+			int mid = (low + high) / 2;
+			if (key < mid)
+				high = mid - 1;
+			else if (key == mid)
+				return mid;
+			else
+				low = mid + 1;
 		}
+		return -low - 1;
+	}
 
-		//Returns binarySearch
+	//selectionSort
+	public static void selectionSort(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			// Find the mimum in the array
+			int min = array[i];
+			int minIndex = i;
 
-		private static int binarySearch(int[] array, int key) {
-			int low = 0;
-			int high = array.length - 1;
-			
-			while (high >= low) {
-				int mid = (low + high) / 2;
-				if (key < mid)
-					high = mid - 1;
-				else if (key == mid)
-					return mid;
-				else
-					low = mid + 1;
+			for (int j = i + 1; j < array.length; j++) {
+				if (min > array[j]) {
+					min = array[j];
+					minIndex = j;
+				}
 			}
-			return -low - 1;
-		}
-		
-       //selectionSort
-		public static void selectionSort(int[] array) {
-			for (int i = 0; i < array.length; i++) {
-				// Find the mimum in the array
-				int min = array[i];
-				int minIndex = i;
 
-				for (int j = i + 1; j < array.length; j++) {
-					if (min > array[j]) {
-						min = array[j];
-						minIndex = j;
-					}
-				}
-
-				// Swap array[i] with array[minIndex] if necessary
-				if (minIndex != i) {
-					array[minIndex] = array[i];
-					array[i] = min; 
-				}
+			// Swap array[i] with array[minIndex] if necessary
+			if (minIndex != i) {
+				array[minIndex] = array[i];
+				array[i] = min; 
 			}
 		}
 	}
-		
 
-	}
 
 }
